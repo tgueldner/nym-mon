52cdad416c599424ed94c5e8fc384c888401630b
tgueldner <t.gueldner@gmx.de>
2022-04-12 00:18:58 +0200

    add nym description port

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from threading import Thread
 import requests
 import yaml
 from keys.telegram import TELEGRAM_CHAT_ID, TELEGRAM_TOKEN
-from keys.nym import NYM_HOST
+from keys.nym import NYM_HOST, NYM_DESC_PORT
 
 logging_yaml_path = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "resources", "logging_config.yaml")
@@ -22,17 +22,17 @@ def setup_logging():
         logging.config.dictConfig(config)
 
 
-def checkState(host):
-    desc_url = "http://{}:8000/description".format(host)
+def checkState(host, port):
+    desc_url = "http://{}:{}/description".format(host, port)
     logger.debug("Check URL: {}".format(desc_url))
     try:
         desc_request = requests.get(desc_url)
         if desc_request.status_code != 200:
-            logger.error("NYM node {} seems to be offline".format(host))
+            logger.error("NYM node {}:{} seems to be offline".format(host))
         else:
             logger.debug("NYM node online!")
     except Exception as err:
-        logger.error("NYM node {} seems to be offline".format(host))
+        logger.error("NYM node {}:{} seems to be offline".format(host))
 
 
 def worker(host, sleep_time):
@@ -46,5 +46,5 @@ if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
     logger.info("Monitoring NYM node at {} now!".format(NYM_HOST))
-    t = Thread(target=worker, args=(NYM_HOST, 60, ))
+    t = Thread(target=worker, args=(NYM_HOST, NYM_DESC_PORT, 60, ))
     t.start()
